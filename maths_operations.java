c39726364899b6136f7af81054155d003407b8a6
ravibalebilalu <81ravikiran@gmail.com>
2023-08-07 13:46:52 +0530

    math and string

---
+++ maths_operations.java
@@ -0,0 +1,27 @@
+public class maths_operations {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 40;
+
+        System.out.println(x + y);
+        System.out.println(y - x);
+        System.out.println(x * y);
+        System.out.println(x/y);
+
+        double result = y/x;
+        System.out.println(result);
+
+        double xd = 30;
+        double yd = 40;
+        double result_d = yd/xd;
+        System.out.println(result_d);
+
+        int a = 33;
+        System.out.println(a%5);
+
+        String one = "Hellow";
+        String two = "World";
+        System.out.println(one + " " +two);
+
+    }
+}

b1e28dc8e90a97ebe73e02ba91ac1abf7c3eadc8
ravibalebilalu <81ravikiran@gmail.com>
2023-08-07 15:58:06 +0530

    variables

--- variable.java
+++ variable.java
@@ -1,11 +1,17 @@
 public class variable{
     public static void main(String[] args) {
         
+        //string
         String chapter = "java";
+        //integer
         int chapter_number = 3;
+        //doubble
         double score = 4.5;
+        //float
         float temperature = 26.543f;
+        //boolean
         boolean is_regular = true;
+        //character
         char grade = 'A';
 
         System.out.println(chapter);

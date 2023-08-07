2e5bfe06cbb21387f6fde3d5bbaf2d2a9bd2d9c7
ravibalebilalu <81ravikiran@gmail.com>
2023-08-07 12:07:31 +0530

    hello world

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,7 @@
 public class hello_world{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Java is awesome");
+        System.out.println(450 * 2);
     }
 }

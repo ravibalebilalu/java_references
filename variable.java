786c74ec64f17fa106fb308312e3e3ac95c01637
ravibalebilalu <81ravikiran@gmail.com>
2023-08-07 13:09:50 +0530

    variable

---
+++ variable.java
@@ -0,0 +1,18 @@
+public class variable{
+    public static void main(String[] args) {
+        
+        String chapter = "Java";
+        String mentor = "chatgpt";
+        int chapter_number = 2;
+        double score = 3.5;
+        float temperature = 25.563f;
+        char grade = 'A';
+        boolean isRegular = true;
+        System.out.println(chapter);
+        System.out.println(mentor);
+        System.out.println(chapter_number);
+        System.out.println(score);
+        System.out.println(temperature);
+        System.out.println(grade);
+    }
+}
